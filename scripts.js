7d079f19542d1b497a10b877d45f7fcfd8d44cde
iandauphinais <ian.dauphin@gmail.com>
2021-05-15 02:47:04 -0400

    Added function game() and tweaked previous functions

--- scripts.js
+++ scripts.js
@@ -1,24 +1,50 @@
 let options = ['rock', 'paper', 'scissors'];
-let playerSelection = prompt("Please enter 'Rock' 'Paper' or 'Scissors'", '');
-let computerSelection = computerPlay();
 
 function computerPlay() {
     let computerPick = options[Math.floor(Math.random() * (options.length))]
-
-    console.log(computerPick);
+    return computerPick;
 }
 
-function playRound(playerSel, computerSel) {
+function playRound() {
+    let playerSel = prompt("Please enter 'Rock' 'Paper' or 'Scissors'", '');
+    let computerSel = computerPlay();
     if (playerSel.toLowerCase() == computerSel) {
-        alert('tie');
+        return 'tie';
     } else if (playerSel.toLowerCase() == 'scissors' && computerSel == 'paper' ||
                playerSel.toLowerCase() == 'rock' && computerSel == 'scissors' ||
                playerSel.toLowerCase() == 'paper' && computerSel == 'rock'){
-        alert('you win')
-    } else (playerSel.toLowerCase() == 'paper' && computerSel == 'scissors' ||
+        return 'win';
+    } else if (playerSel.toLowerCase() == 'paper' && computerSel == 'scissors' ||
             playerSel.toLowerCase() == 'scissors' && computerSel == 'rock' ||
             playerSel.toLowerCase() == 'rock' && computerSel == 'paper'){
-        alert('you lose')
+        return 'lose';
+    } else console.log('Something went wrong');
+}
+
+function game() {
+    let pScore = 0;
+    let cScore = 0;
+    while ((pScore < 5) && (cScore < 5)) {
+        let roundResult = playRound();
+        if (roundResult == 'win'){
+            pScore++;
+            console.log('win','Player Score:',pScore);
+        } else if (roundResult == 'lose') {
+            cScore++;
+            console.log('lose','Computer Score:',cScore);
+        } else if (roundResult == 'tie') {
+            console.log('tie');
+        } else {
+            console.log('Something went wrong');
+        }
+    }
+
+    if (pScore == 5) {
+        console.log("You Win!");
+    } else if(cScore == 5) {
+        console.log("Computer Wins... :(")
+    } else {
+        console.log("Something went wrong")
     }
 }
 
